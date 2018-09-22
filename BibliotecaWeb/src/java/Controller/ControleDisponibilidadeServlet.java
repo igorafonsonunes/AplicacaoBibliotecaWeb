362c71959df0cd2f9cd9356b02de0a51c02bd706
Modesto <igorafonsonunes@outlook.com>
2018-09-22 15:12:05 -0300

    criando pastas para serem salvas as views

--- BibliotecaWeb/src/java/Controller/ControleDisponibilidadeServlet.java
+++ BibliotecaWeb/src/java/Controller/ControleDisponibilidadeServlet.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Controller;
 
 import Model.ManipulaArquivo;
@@ -21,7 +17,7 @@ import javax.swing.JOptionPane;
  * @author Leonardo
  */
 @WebServlet(name = "ControleDisponibilidade", urlPatterns = {"/ControleDisponibilidade"})
-public class ControleDisponibilidade extends HttpServlet {
+public class ControleDisponibilidadeServlet extends HttpServlet {
 
      @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
