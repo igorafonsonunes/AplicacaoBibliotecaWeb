46bdd942e8daf6379cf54875857e185aa3b41000
Modesto <igorafonsonunes@outlook.com>
2018-09-29 15:10:31 -0300

    Alterações no LEIAME

--- BibliotecaWeb/src/java/Controller/LEIAMEServlet.java
+++ BibliotecaWeb/src/java/Controller/LEIAMEServlet.java
@@ -1,5 +1,3 @@
-
-//Igor
 /*Está é uma classe que criei apenas para mostrar que 
 devmos criar todos os Servlets ou JSP dentro do pacote Controller 
 elas devem conter o nome+JSP ou nome+Servlet para nossa organização.
