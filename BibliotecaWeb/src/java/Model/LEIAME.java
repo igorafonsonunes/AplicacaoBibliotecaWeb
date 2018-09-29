1729b2962f62ffe98514782905eb1be3defe77ce
Modesto <igorafonsonunes@outlook.com>
2018-09-29 14:54:06 -0300

    Projeto recriado.

---
+++ BibliotecaWeb/src/java/Model/LEIAME.java
@@ -0,0 +1,12 @@
+/*
+-Está classe criei para demonstrar que dentro do pacote model é onde 
+vamos colocar todas as nossas classes javas que possuem algum tipo de regra de negocio 
+seja ela para ler arquivos ou ela para fazer validaçoes.
+
+- Pessoal vamos tentar deixar bem organizados para que nós não fiquemos perdidos quando formos passar
+alguma informação para a equipe ou até mesmo que fique fácil a leitura dos codigos. 
+
+- Se puder tente criar no maximo apenas métodos genéricos para que todos consigam usar, se não tiver como sem problemas
+mas tente ao maximo criarem métodos genéricos 
+ */
+
