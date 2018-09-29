ba737dbe47c4fb6fbfea552c8e57571022adaf34
Modesto <igorafonsonunes@outlook.com>
2018-09-29 16:48:48 -0300

    Criação do método LerArquivos

--- BibliotecaWeb/src/java/Model/ManipulaArquivos.java
+++ BibliotecaWeb/src/java/Model/ManipulaArquivos.java
@@ -1,17 +1,26 @@
 package Model;
 
+import java.io.*;
+
 /**
- *
  * @author igormodesto
  */
 public class ManipulaArquivos {
-/*A ideia dessa classe é tentar criar todos os métodos o mais genérico possível 
-para que todos possam reaproveitar os códigos descritos */  
+
+    /*A ideia dessa classe é tentar criar todos os métodos o mais genérico possível 
+para que todos possam reaproveitar os códigos descritos */
     
-    
-    
-    
-    
-    
-    
+    public String LerArquivos(String caminhoArquivo, String nomeArquivo) throws FileNotFoundException, IOException {
+        //Declarando variaveis
+        String conteudoArquivo = caminhoArquivo + nomeArquivo;
+        String arquivo = null;
+
+        //Instâncias
+        BufferedReader br = new BufferedReader(new FileReader(conteudoArquivo));
+
+        while (br.ready()) {
+            arquivo += br.readLine() + "|";
+        }
+        return arquivo;
+    }
 }
