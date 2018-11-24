885e09d2723fb7ba67e63de4a4cd397f548d75a8
JeanMudesto <jeanpedromda@hotmail.com>
2018-11-23 23:07:36 -0200

    Lendo na tela o Cpf de acesso

--- BibliotecaWeb/src/java/Controller/Servlet/LoginClienteFuncServlet.java
+++ BibliotecaWeb/src/java/Controller/Servlet/LoginClienteFuncServlet.java
@@ -23,6 +23,9 @@ public class LoginClienteFuncServlet extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
 
         PrintWriter out = response.getWriter();
+        String nome = request.getParameter("nome");
+        String email = request.getParameter("email");
+        String tipo = request.getParameter("tipo");
         String cpf = request.getParameter("cpf");
         String senha = request.getParameter("senha");
         String linha = cpf + ";" + senha;
@@ -33,13 +36,16 @@ public class LoginClienteFuncServlet extends HttpServlet {
             String lerCadastro = ma.LerArquivos("JeanMudesto", "CadastroClienteFuncionario.txt");
 
             if ((RetorneComparacao(lerCadastro, cpf, senha))) {
-                out.println("<h2>Acesso feito com sucesso</h2>");
+               
+            out.println("<h2>Acesso feito com sucesso</h2>");
+                response.setCharacterEncoding("UTF-8");
+                response.getWriter().print("Seu CPF de acesso é :" + cpf);
             } else {
                 out.println("<h2>CPF ou Senha estão errados</h2>");
             }
         } catch (Exception e) {
             out.println(e.getMessage());
-        }
+        } 
 
     }//</editor-fold>
 
@@ -49,10 +55,23 @@ public class LoginClienteFuncServlet extends HttpServlet {
         for (int x = 0; x < conteudo.length; x++) {
             String[] valor = conteudo[x].split(";");
             String c = valor[4];
+            
+            
             if (valor[2].contains(cpf) && c.contains(senha)) {
+                
                 entrar = true;
             }
         }
+        
     return entrar;
     }
+   public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+   
+       System.out.println("Chamando sv");
+       super.service(request, response);
+   }
+   public void init(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+       System.out.println("Chamando sv");
+       super.init();
+   }
 }
